a3dd3f2c9d1f5d5a8f8b8b52ca39106e6745114f
xandros15 <xandros92@gmail.com>
2016-06-23 21:16:54 +0200

    !Draft delete iterator

--- src/Pagination.php
+++ src/Pagination.php
@@ -8,6 +8,7 @@
 
 namespace Xandros15\SlimPagination;
 
+use Slim\Collection;
 use Slim\Http\Request;
 use Slim\Router;
 
@@ -24,10 +25,21 @@ class Pagination
     private $router;
     /** @var int */
     private $current;
-    /** @var Iterator */
+    /** @var Collection */
     private $iterator;
     /** @var array */
     private $options;
+    /** @var int */
+    private $page;
+    /** @var array */
+    private $list;
+    /** @var int */
+    private $max;
+    /** @var int */
+    private $show;
+    private $start;
+    private $end;
+    private $data;
 
     public function __construct(Request $request, Router $router, array $options)
     {
@@ -78,15 +90,7 @@ class Pagination
 
     private function initPages()
     {
-        $this->iterator = new Iterator([
-            'name' => $this->options[self::OPT_NAME],
-            'router' => $this->router,
-            'request' => $this->request,
-            'max' => $this->options[self::OPT_MAX],
-            'current' => $this->current,
-            'show' => $this->options[self::OPT_SHOW],
-            'type' => $this->options[self::OPT_TYPE],
-        ]);
+        $this->iterator = new Collection();
     }
 
     public function getIterator()
@@ -96,7 +100,7 @@ class Pagination
 
     public function isCreatable() : bool
     {
-        return $this->iterator->count() > 1;
+        return true;
     }
 
     public function previous() : PageInterface
@@ -117,5 +121,105 @@ class Pagination
     public function last() : PageInterface
     {
         return $this->iterator->get($this->options[self::OPT_MAX]);
+    }
+
+    /**
+     * Iterator constructor.
+     * @param $data
+     */
+    public function make($data)
+    {
+        $collection = new Collection();
+        $this->show = $data['show'];
+        $this->max = $data['max'];
+        $data['paramName'] = $data['name'];
+        unset($data['name']);
+        unset($data['show']);
+        unset($data['max']);
+        $this->setStartEnd($data);
+        $this->data = $data;
+
+    }
+
+    private function setStartEnd(array $data)
+    {
+        $offset = (int) ($this->show / 2.1);
+        $start = $data['current'] - $offset; // current - edge length - 1 'cuz start on 0
+        $end = $data['current'] + $offset;
+
+        if ($end > $this->max) { // if wanna edge maximum is very last element
+            $end = $this->max;
+            $start = $this->max - $this->show + 1;
+        } elseif ($start < 1) { // if wanna edge minimum is very first element
+            $start = 1;
+            $end = $this->show;
+        }
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    public function toArray()
+    {
+        return $this->compile();
+    }
+
+    private function compile(): array
+    {
+        $list = [];
+
+        for ($current = $this->start; $current <= $this->end; $current++) {
+            $list[] = Factory::create($this->data + [
+                    'pageNumber' => $current,
+                    'pageName' => $current
+                ]);
+        }
+        /** @var $next PageInterface */
+        /** @var $previous PageInterface */
+        /** @var $first PageInterface */
+        /** @var $last PageInterface */
+        $sideControls = $this->createPreviousAndNext($this->data);
+        $edgeControls = $this->createFirstAndLast($this->data);
+
+
+        if ($edgeControls['first']->pageNumber < $this->start) {
+            array_unshift($list, $edgeControls['first']);
+        }
+        if ($edgeControls['last']->pageNumber > $this->end) {
+            $list[] = $edgeControls['last'];
+        }
+
+        array_unshift($list, $sideControls['previous']);
+        $list[] = $sideControls['next'];
+
+        return $list;
+    }
+
+    private function createPreviousAndNext(array $data) : array
+    {
+        return [
+            'previous' => Factory::create($data + [
+                    'pageNumber' => max(1, $data['current'] - 1),
+                    'pageName' => '&lt;'
+                ]),
+            'next' => Factory::create($data + [
+                    'pageNumber' => min($data['current'] + 1, $this->max),
+                    'pageName' => '&gt;'
+                ])
+        ];
+
+    }
+
+    private function createFirstAndLast(array $data) : array
+    {
+        return [
+            'first' => Factory::create($data + [
+                    'pageNumber' => 1,
+                    'pageName' => 1
+                ]),
+            'last' => Factory::create($data + [
+                    'pageNumber' => $this->max,
+                    'pageName' => $this->max
+                ])
+        ];
     }
 }
